efa4337c0bdcf851273f381174d528eea4a80b73
mackenzie <mackenziebligh@gmail.com>
2017-01-29 15:12:56 -0700

    So close q4; unwanted dot product

--- q4.lisp
+++ q4.lisp
@@ -1,8 +1,8 @@
 (defun split (L)
     (cond ((NULL L) '(() ()))
           ((NULL (cdr L)) (list (car L) '()))
-          (t (list (list (car L) (car (split (cddr L))))
-                   (list (car (cdr L)) (car (cdr ((cddr L)))))))
+          (t (list (cons (car L) (car (split (cddr L))))
+                   (cons (car (cdr L)) (car (cdr (split (cddr L)))))))
                     
     )
 )
