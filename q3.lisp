436735ed37bcefc095c975abf534d48958fdff2c
mackenzie <mackenziebligh@gmail.com>
2017-01-29 12:14:34 -0700

    Fix q3. Is 100% correct now

--- q3.lisp
+++ q3.lisp
@@ -4,3 +4,11 @@
           (t (cons (car X) (mix Y (cdr X))))
     )
 )
+
+
+(defun mix (X Y) ; Alternate
+    (cond ((and (NULL X) (NULL Y)) NIL)
+          ((NULL X) Y)
+          (t (cons (car Y) (mix (cdr Y) X )))
+    )
+)
