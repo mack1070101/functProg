8076fe9c997473fa8f37d0580d04618a9adf4785
mackenzie <mackenziebligh@gmail.com>
2017-01-31 14:20:29 -0700

    Add prune to q6

--- q6.lisp
+++ q6.lisp
@@ -1,11 +1,13 @@
-(defun sumlist (L)
-    (if (null L)
-        0
-        (+ (car L) (sumlist (cdr L)))
+(defun pruneList (S L)
+    (cond ((NULL L) L)  ; If empty return the empty list
+          ((and (NULL (cdr L)) (< (car L) S)) (list (car L))) ; If it is a single iteme list, and the item is less than S, return it
+          ((< (car L) S) (append (list (car L)) (pruneList S (cdr L)))) ; Build a list of items that are less than sum 
     )
 )
 
 (defun subsetsum (S L)
-    (cond ((AND (NULL (cdr L)) (not (= s (car L)))) NIL)
+    (cond ((= s 0) L) ; If s = 0, return the list
+          ((AND (NULL L) (not (= S 0))) nil) ; If the list is empty, and sum != 0, return nil
+          (t ((subsetsum)))
     )
 )
