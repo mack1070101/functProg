c28de80a093305f172e3e7994ff96621ff25da23
mackenzie <mackenziebligh@gmail.com>
2017-02-01 13:15:05 -0700

    Finish q6

--- q6.lisp
+++ q6.lisp
@@ -4,15 +4,22 @@
     )
 )
 
+(defun subsethelper (S L)
+    (let* (
+        (saved (subsetsum (- S (car L)) (cdr L)))
+    )
+    (cond (saved (cons (car L) saved))
+          (t (subsetsum S (cdr L)))
+    )
+    )
+)
+
 (defun subsetsum (S L)
   ; should be sorting, and summing check for correctness
     (cond ((OR (< S 0) (NULL L)) NIL)
           ((> (car(sort (copy-list L) '<)) S) NIL);List cannot contain the subset because smallest > S
           ((< (sumlist L) S) NIL) ; List cannot sum to correct value because it's too small
           ((= S (car L)) (list (car L)))
-          ((subsetsum (- S (car L)) (cdr L)) (cons (car L) (subsetsum (- S (car L)) (cdr L)))) ; Add let statment to improve computation
-          (t (subsetsum S (cdr L)))
+          (t ( subsethelper S L))
     )
 )
-
-; Cannot create a let conditon inside a cond → create a wrapper of the bottom two lines
