53927e2e8ee45bc207c9c8c6cc83126191f8ef84
mackenzie <mackenziebligh@gmail.com>
2017-02-01 12:16:45 -0700

    Non optimized answer for q6

--- q6.lisp
+++ q6.lisp
@@ -3,17 +3,6 @@
           (t (+ (car x) (sumList (cdr x))))
     )
 )
-(defun lastx (x)
-    (cond ((NULL (cdr X)) (car x))
-          (t (last (cdr x)))
-    )
-)
-
-(defun listwithoutlast (x)
-    (cond ((NULL (cdr x)) nil)
-          (t(append (list (car x)) (listwithoutlast (cdr x))))
-    )
-)
 
 (defun subsetsum (S L)
   ; should be sorting, and summing check for correctness
@@ -21,15 +10,9 @@
           ((> (car(sort (copy-list L) '<)) S) NIL);List cannot contain the subset because smallest > S
           ((< (sumlist L) S) NIL) ; List cannot sum to correct value because it's too small
           ((= S (car L)) (list (car L)))
-          (t (append (subsetsum S (cdr L)) (subsetsum (- S (car L)) (cdr L))))
+          ((subsetsum (- S (car L)) (cdr L)) (cons (car L) (subsetsum (- S (car L)) (cdr L)))) ; Add let statment to improve computation
+          (t (subsetsum S (cdr L)))
     )
 )
 
-; Want to create a binary tree with your recursion
-
-;first bc checks for equality between 1st element of 1st element of sorted listkkkkk
-; check if sum of list is < L = null
-; Check if sorted list has 1st element > sum = null
-
-
-((> (CAR (SORT '(1 48 18 21) '<)) 4))
+; Cannot create a let conditon inside a cond → create a wrapper of the bottom two lines
