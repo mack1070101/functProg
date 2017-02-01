b9534a84971871fba0c6a5fd20bd346ecad2556d
mackenzie <mackenziebligh@gmail.com>
2017-01-31 20:45:50 -0700

    Origial q4

--- q4.lisp
+++ q4.lisp
@@ -1,7 +1,7 @@
 (defun split (L)
     (cond ((NULL L) '(() ()))
-          ((NULL (cdr L)) (list (car L) '()))
-          (t (list (append (list(car L)) (list(car (split (cddr L)))))
+          ((NULL (cdr L)) (list (car L) ))
+          (t (list (cons (car L) (car (split (cddr L)))) ; Add nested if call to check (car (split (cddr L))) for atomicity
                    (cons (car (cdr L)) (car (cdr (split (cddr L)))))))
     )
 )
